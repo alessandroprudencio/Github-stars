0265c706e90572c3b95e54ade0762b8aa6362368
alessandroprudencio <alessandroconectado@gmail.com>
2019-02-12 03:15:26 -0200

    Criando diretórios e registrando primeiro componente

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <areaPesquisa/>
+
+  </div>
+</template>
+
+<script>
+import areaPesquisa from "@/components/area-superior/area-pesquisa"
+
+export default {
+  name: 'App',
+  components:{areaPesquisa}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
